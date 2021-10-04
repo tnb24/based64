cfbe4b7cf006521a4fb0e7f90db2974c6fc0c5b6
tnb24 <tanaybhardwaj24@gmail.com>
2021-10-04 16:54:19 +0000

    pls work

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const sha256 = require("js-sha256");
 const sha512 = require("js-sha512");
+const argon2 = require("argon2");
+
+async function hash_it(str) {
+    try {
+        const hash = await argon2.hash(str);
+        const newHash = await hash.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\]\\]/gi, "");
+        const ultraNewHash = await newHash.replace("argon"," ");
+        console.log(ultraNewHash);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 module.exports = (req,res) => {
     const randomStr = Math.random()
@@ -7,5 +19,7 @@ module.exports = (req,res) => {
         .replace(".", "");
     const moreRandomStr = sha256(randomStr);
     const ultraRandomStr = sha512(moreRandomStr).replace("."," ");
+    const test = hash_it(ultraRandomStr);
+    console.log(test);
     res.send(`${ultraRandomStr}`);
 };
