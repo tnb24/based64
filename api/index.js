d178a7e8faf9d7bce2fdd6ccb46ba154604da0c2
Tanay Bhardwaj <tanaybhardwaj24@gmail.com>
2021-10-04 20:59:36 +0530

    init

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const sha256 = require("js-sha256");
+const sha512 = require("js-sha512");
+
+module.exports = (req,res) => {
+    const randomStr = Math.random()
+        .toString(36)
+        .replace(".", "");
+    const moreRandomStr = sha256(randomStr);
+    const ultraRandomStr = sha512(moreRandomStr).replace(".","");
+    res.send(`${ultraRandomStr}`);
+};
