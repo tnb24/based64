708be06123bcb65305cc7204279ed0c2b60a2e63
tanay <70639071+tnb24@users.noreply.github.com>
2021-10-04 21:30:12 +0530

    removed removing '.'

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 const sha256 = require("js-sha256");
 const argon2 = require("argon2");
-// const sha512 = require("js-sha512");
 
 module.exports = (req,res) => {
     const randomStr = Math.random()
         .toString(36)
         .replace(".", "");
     const moreRandomStr = sha256(randomStr);
-    const ultraRandomStr = argon2.hash(moreRandomStr).replace("."," ");
+    const ultraRandomStr = argon2.hash(moreRandomStr) //.replace("."," ");
     res.send(`${ultraRandomStr}`);
 };
