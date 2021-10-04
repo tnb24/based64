291f48b32678947a3ae379285398179e8a05dff5
Tanay Bhardwaj <tanaybhardwaj24@gmail.com>
2021-10-04 21:24:12 +0530

    added new hash

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const sha256 = require("js-sha256");
-const sha512 = require("js-sha512");
+const argon2 = require("argon2");
+// const sha512 = require("js-sha512");
 
 module.exports = (req,res) => {
     const randomStr = Math.random()
         .toString(36)
         .replace(".", "");
     const moreRandomStr = sha256(randomStr);
-    const ultraRandomStr = sha512(moreRandomStr).replace(".","");
+    const ultraRandomStr = argon2.hash(moreRandomStr).replace("."," ");
     res.send(`${ultraRandomStr}`);
 };
