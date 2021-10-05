6330ad2b75d0582c85ea6f63de02ff52704be9d0
tnb24 <tanaybhardwaj24@gmail.com>
2021-10-05 16:56:51 +0000

    0 libs bouy

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 module.exports = (req,res) => {
-    const randomStr = Math.random()
+    const query = req.query;
+    const randomStr = Math.random(query)
         .toString(36)
         .replace(".", "");
     const moreRandomStr = Math.random(randomStr).toString(36);
-    const ultraRandomStr = Math.random(moreRandomStr).toString(36).replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\]\\]/gi, "");
+    const ultraRandomStr = Math.random(moreRandomStr).toString(36).replace(/[`~!@#$%^&*()_|+\-=?;0:'",.<>\]\\]/gi, "");
     res.send(`${ultraRandomStr}`);
 };
