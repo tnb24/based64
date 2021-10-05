390fbe32f55f30f0e23bfa2c42b9886c417c1511
tanay <70639071+tnb24@users.noreply.github.com>
2021-10-05 10:58:08 +0530

    added regex

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ module.exports = (req,res) => {
         .toString(36)
         .replace(".", "");
     const moreRandomStr = Math.random(randomStr).toString(36);
-    const ultraRandomStr = Math.random(moreRandomStr).toString(36);
+    const ultraRandomStr = Math.random(moreRandomStr).toString(36).replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\]\\]/gi, "");
     res.send(`${ultraRandomStr}`);
 };
