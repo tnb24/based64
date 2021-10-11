bf9c7a6496d1d77f2df43192d9b2178e841241cb
tnb24 <tanaybhardwaj24@gmail.com>
2021-10-11 16:52:33 +0000

    new names

--- api/decode.js
+++ api/decode.js
@@ -1,7 +1,7 @@
 const { Base64 } = require("js-base64");
 
 module.exports = (req,res) => {
-    const query = req.query.name;
-    const decrypted_str = Base64.decode(query)
+    const query = req.query.data;
+    const decrypted_str = Base64.decode(query);
     res.send(`${decrypted_str}`);
 };
--- api/encode.js
+++ api/encode.js
@@ -1,7 +1,7 @@
 const { Base64 } = require("js-base64");
 
 module.exports = (req,res) => {
-    const query = req.query.name;
-    const encrypted_str = Base64.encode(query)
+    const query = req.query.data;
+    const encrypted_str = Base64.encode(query);
     res.send(`${encrypted_str}`);
 };
